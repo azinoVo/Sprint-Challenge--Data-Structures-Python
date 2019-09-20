40652fd4e7493c4856d3d9927aed42c05ef5d65f
azinoVo <azinovo22@gmail.com>
2019-09-20 11:33:08 -0500

    implem names using python sets

--- names/names.py
+++ names/names.py
@@ -13,11 +13,18 @@ f.close()
 # so total complexity is about 0(n^2) which is pretty huge since n is 10,000
 # For each of the 10,000, it has to iterate over each of the other 10,000 so 10,000^2
 
-duplicates = []
-for name_1 in names_1: # n
-    for name_2 in names_2: # n
-        if name_1 == name_2: # 0(1)
-            duplicates.append(name_1)
+# duplicates = []
+# for name_1 in names_1: # n
+#     for name_2 in names_2: # n
+#         if name_1 == name_2: # 0(1)
+#             duplicates.append(name_1)
+
+# Python seems to have a bunch of methods for comparing two sets of data, returning differences and similarities
+# Can I use this?
+
+# duplicates = names_1.intersection(names_2)
+duplicates = list(set(names_1) & set(names_2))
+print("SET?", duplicates)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
